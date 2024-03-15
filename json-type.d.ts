7e8c3f3fb8d0827226d02ae47d3e09efafc695e7
shaman-apprentice <Torsten.Knauf@gmx.de>
2024-03-15 21:10:44 +0100

    feat: Add JSONValue type

---
+++ json-type.d.ts
@@ -0,0 +1,10 @@
+export type JSONValue = string
+  | number
+  | boolean
+  | null
+  | JSONObject
+  | JSONArray;
+
+export type JSONObject = { [key: string]: JSONValue };
+
+export type JSONArray = JSONValue[];
